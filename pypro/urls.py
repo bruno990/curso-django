4a7c994b25b0f815326b487fc59fb5f77eb9261d
br-romero@hotmail.com <br-romero@hotmail.com>
2021-06-01 22:20:30 -0300

    Adicionado Django debug tollbar
    Close #28

--- pypro/urls.py
+++ pypro/urls.py
@@ -16,7 +16,15 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from pypro import settings
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('pypro.base.urls')),
 ]
+
+if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns.append(
+        path('__debug__/', include(debug_toolbar.urls))
+    )
